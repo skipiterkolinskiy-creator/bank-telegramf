7d71110779c26779e3d0b4553760723233287c12
PixelPivo <skipiterkolinskiy@gmail.com>
2026-07-14 00:18:13 +0300

    Add files via upload

--- handlers/start.py
+++ handlers/start.py
@@ -5,15 +5,12 @@ from aiogram.filters import CommandStart
 from aiogram.types import CallbackQuery, Message
 
 from keyboards.menu import main_menu
+from utils.bank_format import account_tail, money
 from utils.database import Database
 from utils.ui import answer_callback, replace_menu
 
 
 router = Router()
-
-
-def money(value: float) -> str:
-    return f"{value:,.2f}".replace(",", " ")
 
 
 def main_text(user: dict, is_new: bool = False) -> str:
@@ -33,6 +30,7 @@ def main_text(user: dict, is_new: bool = False) -> str:
         "🏦 <b>Z-Bank | Главное меню</b>\n\n"
         f"Клиент: <b>{user['name']}</b>\n"
         f"Z-ID: <code>{user['passport']}</code>\n"
+        f"Счет: <code>{account_tail(user)}</code>\n"
         f"Баланс RUB: <b>{money(float(balances.get('RUB', 0.0)))} RUB</b>\n"
         f"USD: <b>{money(float(balances.get('USD', 0.0)))}</b>\n"
         f"EUR: <b>{money(float(balances.get('EUR', 0.0)))}</b>\n"
